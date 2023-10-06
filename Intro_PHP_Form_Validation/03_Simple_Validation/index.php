b04221ce182dfe8c698260b2d53dfec7ca2f042c
Chad <chadb78999@gmail.com>
2023-10-06 07:40:29 -0500

    working on validation

--- Intro_PHP_Form_Validation/03_Simple_Validation/index.php
+++ Intro_PHP_Form_Validation/03_Simple_Validation/index.php
@@ -1,4 +1,44 @@
-<!DOCTYPE HTML>
-<html>
-<p>to do</p>
-</html>
+<?php
+// Define checkWord() here:
+  function checkWord($userInput,$correctInput)
+  {
+	if($_SERVER["REQUEST_METHOD"]==="POST" && strtolower($userInput[0]) !=$correctInput)
+	{
+		return "* This word must start with the letter {$correctInput}!";
+	} else{
+		return "";
+	}
+  }
+  
+  
+  
+  
+?>
+  
+<h1>Time to Practice our ABCs</h1>
+<form method="post" action="">
+    Enter a word that starts with the letter "a":
+    <br>
+    <input type="text" name="a-word" id="a-word">
+    <br>      
+      
+    <br>     
+    Enter a word that starts with the letter "b":
+    <br>
+    <input type="text" id="b-word" name="b-word">
+    <br>      
+      
+    <br>
+    Enter a word that starts with the letter "c":
+    <br>
+    <input type="text" id="c-word" name="c-word">
+    <br>      
+      
+    <br>
+    <input type="submit" value="Submit Words">
+</form>
+<div>
+    <h3>"a" is for: <?= $_POST["a-word"];?><h3>
+    <h3>"b" is for: <?= $_POST["b-word"];?><h3>
+    <h3>"c" is for: <?= $_POST["c-word"];?><h3>    
+<div>  
