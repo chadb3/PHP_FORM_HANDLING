bbf3e5d8e758dfe70e8c1b96db52bd104517d1e8
Chad <chadb78999@gmail.com>
2023-10-08 07:11:08 -0500

    got it working

--- Intro_PHP_Form_Validation/03_Simple_Validation/index.php
+++ Intro_PHP_Form_Validation/03_Simple_Validation/index.php
@@ -1,17 +1,10 @@
 <?php
 $a_word = $b_word = $c_word = "x";
-// Note: I am seeing additional issues/warnings that are not present in the codecademy lessons
-// for example:
-// my inputs have something similar to "<br /><b>Warning</b>:  Undefined array key " in each.
-// and: Warning: Undefined array key "a-word" in ../03_Simple_Validation/index.php on line 29
-echo phpversion(); 
+// I was able to resolve most of the warnings by checking if the values are set
+//echo phpversion(); 
   function checkWord($userInput,$correctInput)
   {
 
-	// my code was having issues and throwing errors.
-	// codecademy may be using an older version of php. 
-	// I will upload the working solution (this one)
-	// then attempt to fix it so it can run on my version.
 	if($_SERVER["REQUEST_METHOD"]==="POST" && strtolower($userInput[0] ?? "") !=$correctInput)
 	{
 		return "* This word must start with the letter {$correctInput}!";
@@ -29,7 +22,7 @@ echo phpversion();
 <form method="post" action="post">
     Enter a word that starts with the letter "a":
     <br>
-    <input type="text" id="a-word" name="a_word" value="<?= $_POST["a_word"];?>">
+    <input type="text" id="a-word" name="a_word" value="<?php  if(isset($_POST["a_word"])){ echo $_POST["a_word"];}?>">
 	<p class="error" id="a-error">
 	<?php
 	if(isset($_POST["a_word"])){echo checkWord($_POST["a_word"],'a');}
@@ -42,23 +35,23 @@ echo phpversion();
     <br>     
     Enter a word that starts with the letter "b":
     <br>
-    <input type="text" id="b-word" name="b_word" value="<?= $_POST["b_word"];?>">
-    <p class="error" id="b-error"><?=checkWord($_POST["b_word"],'b'); ?></p>
+    <input type="text" id="b-word" name="b_word" value="<?php  if(isset($_POST["b_word"])){ echo $_POST["b_word"];}?>">
+    <p class="error" id="b-error"><?php if(isset($_POST["b_word"])){echo checkWord($_POST["b_word"],'b');} ?></p>
     <br>      
       
     <br>
     Enter a word that starts with the letter "c":
     <br>
-    <input type="text" id="c-word" name="c_word" value="<?= $_POST["c_word"];?>">
-    <p class="error" id="c-error"><?=checkWord($_POST["c_word"],'c'); ?></p>
+    <input type="text" id="c-word" name="c_word" value="<?php  if(isset($_POST["c_word"])){ echo $_POST["c_word"];}?>">
+    <p class="error" id="c-error"><?php if(isset($_POST["c_word"])){echo checkWord($_POST["c_word"],'c');} ?></p>
     <br>      
       
     <br>
     <input type="submit" value="Submit Words">
 </form>
 <div>
-    <h3>"a" is for: <?= $_POST["a_word"];?><h3>
-    <h3>"b" is for: <?= $_POST["b_word"];?><h3>
-    <h3>"c" is for: <?= $_POST["c_word"];?><h3>    
+    <h3>"a" is for: <?php  if(isset($_POST["b_word"])){ echo $_POST["b_word"];}?><h3>
+    <h3>"b" is for: <?php  if(isset($_POST["b_word"])){ echo $_POST["b_word"];}?><h3>
+    <h3>"c" is for: <?php  if(isset($_POST["c_word"])){ echo $_POST["c_word"];}?><h3>    
 <div>  
 <a href="index.php">Reset</a>
