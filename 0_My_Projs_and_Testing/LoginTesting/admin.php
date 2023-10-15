817c4b3d13beb41b5849cfc9b0494333e051d42f
Chad <chadb78999@gmail.com>
2023-10-15 07:01:47 -0500

    still working on basic user auth

--- 0_My_Projs_and_Testing/LoginTesting/admin.php
+++ 0_My_Projs_and_Testing/LoginTesting/admin.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
-echo "session start". " ";
-echo $_SESSION["username"]." ".$_SESSION["count"]." ".$_SESSION["permission"];
+//echo "session start". " ";
+//echo $_SESSION["username"]." ".$_SESSION["count"]." ".$_SESSION["permission"];
+if($_SESSION["permission"]!=="admin")
+{
+	header('HTTP/1.1 403 Forbidden');
+	echo "\n<h1>Access Denied!</h1>";
+}
 ?><!DOCTYPE html>
 <html>
 <head>
