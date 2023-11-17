bc37e4c2985b5dc46ecf48609bbf89655552bfbb
Chad B <chadb78999@gmail.com>
2023-11-17 07:48:53 -0600

    working on styling on edit user

--- 0_My_Projs_and_Testing/LoginTesting/edit_user.php
+++ 0_My_Projs_and_Testing/LoginTesting/edit_user.php
@@ -8,8 +8,14 @@ if($_SESSION["user"]->getPermissionLevel()!=="admin")
 {
 	header('HTTP/1.1 403 Forbidden');
 	echo "\n<h1>Access Denied!</h1>";
-}else{echo "<h3>Edit User: <i>{$_GET["name"]}</i></h3>";
+}else{
 ?><!DOCTYPE html>
-
+<head>
+	<title>Editing: <?= $_GET["name"]; ?></title>
+	<style>
+		h1{display:inline;padding-right:.5em;}
+	</style>
+</head>
+<?php echo "<h1>Edit User:</h1>    <i>{$_GET["name"]}</i>";?>
 <?php }?>
 </html>
